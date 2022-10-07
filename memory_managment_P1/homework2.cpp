bbfabf309dee31f529844c92e28e26ef5a1ce2c2
Mohammad Norouzifard <Mohammad.Norouzifard@yoobeecolleges.com>
2022-10-07 17:02:08 +1300

    first version

---
+++ memory_managment_P1/homework2.cpp
@@ -0,0 +1,7 @@
+#include "homework2.h"
+
+homework2::homework2(QWidget *parent)
+    : QWidget{parent}
+{
+
+}
---
+++ memory_managment_P1/homework2.h
@@ -0,0 +1,16 @@
+#ifndef HOMEWORK2_H
+#define HOMEWORK2_H
+
+#include <QWidget>
+
+class homework2 : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit homework2(QWidget *parent = nullptr);
+
+signals:
+
+};
+
+#endif // HOMEWORK2_H
